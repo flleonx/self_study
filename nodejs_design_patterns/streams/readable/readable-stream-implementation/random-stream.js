003181196806f9c9561fd54b4508259fe31e4f2d
flleonx <flleondev@gmail.com>
2022-09-04 13:33:03 -0500

    readable streams

--- nodejs_design_patterns/streams/readable/readable-stream-implementation/random-stream.js
+++ nodejs_design_patterns/streams/readable/readable-stream-implementation/random-stream.js
@@ -5,18 +5,18 @@ const chance = new Chance();
 
 export class RandomStream extends Readable {
   constructor(options) {
-    super(options)
-    this.emittedBytes = 0
+    super(options);
+    this.emittedBytes = 0;
   }
 
   _read(size) {
     const chunk = chance.string({ length: size });
-
     this.push(chunk, 'utf8');
+
     this.emittedBytes += chunk.length;
 
     if (chance.bool({ likelihood: 5 })) {
-      this.push(null);
+      this.push(null)
     }
   }
 }
