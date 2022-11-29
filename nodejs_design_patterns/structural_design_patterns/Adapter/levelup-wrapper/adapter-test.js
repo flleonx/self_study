f4b3595897b457dc3bc2868eed59cf9f17ddb871
flleonx <flleondev@gmail.com>
2022-11-28 23:34:34 -0500

    adapter pattern reviewed

---
+++ nodejs_design_patterns/structural_design_patterns/Adapter/levelup-wrapper/adapter-test.js
@@ -0,0 +1,27 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { Level } from 'level';
+import { createFSAdapter } from './fs-adapter.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const db = new Level(join(__dirname, 'db'), {
+  valueEncoding: 'binary'
+})
+
+const fs = createFSAdapter(db);
+
+fs.writeFile('file.txt', 'Hello!', () => {
+  fs.readFile('file.txt', { encoding: 'utf8' }, (err, res) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(res);
+  })
+})
+
+// try to read a missing file
+fs.readFile('missing.txt', { encoding: 'utf8' }, (err, _res) => {
+  console.error(err);
+})
---
+++ nodejs_design_patterns/structural_design_patterns/Adapter/levelup-wrapper/fs-test.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+
+fs.writeFile('file.txt', 'Hello!', () => {
+  fs.readFile('file.txt', { encoding: 'utf8' }, (err, res) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(res);
+  })
+})
+
+// try to read a missing file
+fs.readFile('missing.txt', { encoding: 'utf8' }, (err, _res) => {
+  console.error(err);
+})
