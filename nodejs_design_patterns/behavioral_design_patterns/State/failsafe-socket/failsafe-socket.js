e56a89eeedb226f88225b5c61ed97d9c6300f5b4
flleonx <flleondev@gmail.com>
2022-12-09 01:00:47 -0500

    State pattern reviewed

---
+++ nodejs_design_patterns/behavioral_design_patterns/State/failsafe-socket/failsafe-socket.js
@@ -0,0 +1,26 @@
+import { OfflineState } from "./offline-state.js";
+import { OnlineState } from "./online-state.js";
+
+export class FailsafeSocket {
+  constructor(options) {
+    this.options = options;
+    this.queue = [];
+    this.currentState = null;
+    this.socket = null;
+    this.states = {
+      offline: new OfflineState(this),
+      online: new OnlineState(this),
+    };
+    this.changeState("offline");
+  }
+
+  changeState(state) {
+    console.log(`Activating state: ${state}`);
+    this.currentState = this.states[state];
+    this.currentState.activate();
+  }
+
+  send(data) {
+    this.currentState.send(data);
+  }
+}
