fa4210d87eec2f9be1c9118f038cd9045edababf
flleonx <flleondev@gmail.com>
2022-12-11 21:38:59 -0500

    Iterator pattern in javascript world

---
+++ nodejs_design_patterns/behavioral_design_patterns/Iterator/alphabet-iterator/alphabet-iterator.js
@@ -0,0 +1,28 @@
+const A_CHAR_CODE = 65;
+const Z_CHAR_CODE = 90;
+
+function createAlphabetIterator() {
+  let currCode = A_CHAR_CODE;
+
+  return {
+    next() {
+      const currChar = String.fromCodePoint(currCode);
+
+      if (currCode > Z_CHAR_CODE) {
+        return { done: true };
+      }
+
+      currCode++;
+
+      return { value: currChar, done: false };
+    },
+  };
+}
+
+const iterator = createAlphabetIterator();
+let iterationResult = iterator.next();
+
+while(!iterationResult.value) {
+  console.log(iterationResult.value);
+  iterationResult = iterator.next();
+}
