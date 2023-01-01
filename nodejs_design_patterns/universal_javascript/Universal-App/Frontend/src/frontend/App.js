a9853c131b19ed0d6b9379415f7c2ce28a171743
flleonx <flleondev@gmail.com>
2023-01-01 16:46:03 -0500

    universal javascript: universal-app frontend only

---
+++ nodejs_design_patterns/universal_javascript/Universal-App/Frontend/src/frontend/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import htm from "htm";
+import { Switch, Route } from "react-router-dom";
+
+import { AuthorsIndex } from "./components/pages/AuthorsIndex.js";
+import { Author } from "./components/pages/Author.js";
+import { FourOhFour } from "./components/pages/FourOhFour.js";
+
+const html = htm.bind(React.createElement);
+
+export class App extends React.Component {
+  render() {
+    return html`
+      <${Switch}>
+        <${Route} path="/" exact=${true} component=${AuthorsIndex} />
+        <${Route} path="/author/:authorId" component=${Author} />
+        <${Route} path="*" component=${FourOhFour} />
+      </${Switch}>
+    `;
+  }
+}
