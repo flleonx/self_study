49b24d0822bee4484f3e4082d1b008ed273edcf0
flleonx <flleondev@gmail.com>
2023-01-22 01:22:08 -0500

    asynchronously initialized components

---
+++ nodejs_design_patterns/advanced_recipes/Asynchronously-Initialized-Components/Pre-Initialization-Queues/Leverage-State-Pattern/index.js
@@ -0,0 +1,14 @@
+import { once } from "events";
+import { db } from "./db.js";
+
+db.connect();
+
+async function updateLastAccess() {
+  await db.query(`INSERT (${Date.now()}) INTO "LastAccesses"`);
+}
+
+updateLastAccess();
+
+setTimeout(() => {
+  updateLastAccess();
+}, 3000);
