f7b1f0129f850c498c5440bcc0d55cf018279bd2
flleonx <flleondev@gmail.com>
2023-04-16 22:58:01 -0500

    DSA (stack, queue, arraylist)

--- algorithms/kata-machine/src/day1/ArrayList.ts
+++ algorithms/kata-machine/src/day1/ArrayList.ts
@@ -1,27 +1,62 @@
 export default class ArrayList<T> {
-    public length: number;
+  public length: number;
+  private list: T[];
 
-    
+  constructor(capacity: number) {
+    this.length = 0;
+    this.list = Array(capacity);
+  }
 
-    constructor() {
+  prepend(item: T): void {
+    this.length++;
+
+    for (let i = this.length; i >= 0; i--) {
+      this.list[i] = this.list[i - 1];
     }
 
-    prepend(item: T): void {
+    this.list[0] = item;
+  }
 
-}
-    insertAt(item: T, idx: number): void {
+  insertAt(item: T, idx: number): void {
+    for (let i = idx; i < this.length; i++) {
+      this.list[i + 1] = this.list[i];
+    }
 
-}
-    append(item: T): void {
+    this.length++;
 
-}
-    remove(item: T): T | undefined {
+    this.list[idx] = item;
+  }
 
-}
-    get(idx: number): T | undefined {
+  append(item: T): void {
+    const idx = this.length;
+    this.list[idx] = item;
+    this.length++;
+  }
 
-}
-    removeAt(idx: number): T | undefined {
+  remove(item: T): T | undefined {
+    let idx = undefined;
+    for (let i = 0; i < this.length; i++) {
+      if (this.list[i] === item) {
+        idx = i;
+      }
+    }
 
-}
+    return idx !== undefined ? this.removeAt(idx) : undefined;
+  }
+
+  get(idx: number): T | undefined {
+    return this.list[idx];
+  }
+
+  removeAt(idx: number): T | undefined {
+    const item = this.list[idx];
+
+    for (let i = idx; i < this.length; i++) {
+      this.list[i] = this.list[i + 1];
+    }
+
+    this.length--;
+
+    return item;
+  }
 }
--- algorithms/kata-machine/src/day1/Stack.ts
+++ algorithms/kata-machine/src/day1/Stack.ts
@@ -1,18 +1,51 @@
+type Node<T> = {
+  value: T;
+  prev?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const node: Node<T> = {
+      value: item,
+      prev: undefined,
+    };
+
+    this.length++;
+
+    if (!this.head) {
+      this.head = node;
+      return;
     }
 
-    push(item: T): void {
+    node.prev = this.head;
+    this.head = node;
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    this.length--;
 
-}
+    const node = this.head;
+    this.head = node.prev;
+
+    // "free"
+    node.prev = undefined;
+
+    return node.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
