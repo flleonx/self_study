8ee1a7b3ecd137f7a464e0fa5bc39f5858210a14
flleonx <flleondev@gmail.com>
2025-04-06 13:15:25 +0200

    Writing an interpreter in Go chap:2 - parsing LET statements

---
+++ go_lang/monkey-language/ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "monkey/token"
+
+type (
+	Node interface {
+		TokenLiteral() string
+	}
+
+	Statement interface {
+		Node
+		statementNode()
+	}
+
+	Expression interface {
+		Node
+		expressionNode()
+	}
+
+	Program struct {
+		Statements []Statement
+	}
+
+	Identifier struct {
+		Token token.Token
+		Value string
+	}
+
+	LetStatement struct {
+		Token token.Token // the token.LET token
+		Name  *Identifier
+		Value Expression
+	}
+)
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
