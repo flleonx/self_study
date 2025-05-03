c81169d24a2b687d68b77c3d848bf30c6008f25f
flleonx <flleondev@gmail.com>
2025-05-03 14:38:30 +0200

    WIG: chap2 - parse if expressions

--- go_lang/monkey-language/ast/ast.go
+++ go_lang/monkey-language/ast/ast.go
@@ -71,6 +71,18 @@ type (
 		Operator string
 		Right    Expression
 	}
+
+	IfExpression struct {
+		Token       token.Token // The 'if' token
+		Condition   Expression
+		Consequence *BlockStatement
+		Alternative *BlockStatement
+	}
+
+	BlockStatement struct {
+		Token      token.Token // the { token
+		Statements []Statement
+	}
 )
 
 func (p *Program) TokenLiteral() string {
@@ -170,6 +182,36 @@ func (ie *InfixExpression) String() string {
 	out.WriteString(fmt.Sprintf(" %s ", ie.Operator))
 	out.WriteString(ie.Right.String())
 	out.WriteString(")")
+
+	return out.String()
+}
+
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
 
 	return out.String()
 }
